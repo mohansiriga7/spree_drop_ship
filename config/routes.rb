dad4febb683edf90c5cd908673f189388a1da68d
mohansiriga7 <ms7.developer@gmail.com>
2018-04-12 20:37:11 +0530

    Added '/store' route for store admin to check their store orders(For Authorization Failure Fix)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Spree::Core::Engine.add_routes do
     resource :drop_ship_settings
     resources :shipments
     resources :suppliers
+    spree_path = Rails.application.routes.url_helpers.try(:spree_path, trailing_slash: true) || '/'
+    get "/store", to: redirect((spree_path + Spree.admin_path + '/sub_orders').gsub('//', '/')), as: :admin
   end
-
 end
